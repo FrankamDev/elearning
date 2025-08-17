49e8932ad7b045fc372debff85544a44502e7f44
Frank kamgang <fkamgang06@gmail.com>
2025-08-17 20:58:40 +0100

    color added

--- resources/js/pages/Contact.tsx
+++ resources/js/pages/Contact.tsx
@@ -32,7 +32,7 @@ const Contact = () => {
    <Navbar />
    <div className="min-h-screen bg-gradient-to-br from-blue-200 via-blue-600 to-blue-900 flex flex-col items-center justify-center py-12 px-4">
     <motion.div
-     className="max-w-6xl w-full bg-gray-900 p-10 rounded-3xl shadow-2xl border border-blue-400"
+     className="max-w-6xl w-full bg-gradient-to-br from-blue-900 via-blue-900 to-black p-10 rounded-3xl shadow-2xl border border-blue-400"
      initial={{ opacity: 0, y: 60 }}
      animate={{ opacity: 1, y: 0 }}
      transition={{ duration: 1 }}
@@ -44,7 +44,7 @@ const Contact = () => {
       animate={{ scale: 1, opacity: 1 }}
       transition={{ duration: 0.8 }}
      >
-      Contactez Frankam
+      Contactez FrankamDev
      </motion.h2>
      <motion.p
       className="text-center text-gray-300 mb-12 text-lg"
